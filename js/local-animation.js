7a1d994c30b5cb98bef986be45774960eccc8c24
rajamuthukumara <rajamuthukumaran333@gmail.com>
2018-02-18 19:54:55 +0530

    commit 1

---
+++ js/local-animation.js
@@ -0,0 +1,61 @@
+$(window).scroll(function(){
+  fixednav();
+  // parallax();
+})
+
+function fixednav(){
+    if($(document).scrollTop() > 50){
+      $('nav').addClass('fixed-nav');
+      $('.banner').addClass('fixed-banner');
+    }
+    else {
+      $('nav').removeClass('fixed-nav');
+      $('.banner').removeClass('fixed-banner');
+    }
+}
+
+// function parallax(){
+//   var wScroll = $(this).scrollTop();
+//   $('.parallax').css('background-position','center '+(-wScroll*0.01)+'px');
+// }
+
+// function parallax() {
+//   var scroll = $(window).scrollTop();
+//   var screenHeight = $(window).height();
+//
+//   $('.parallax').each(function() {
+//     var offset = $(this).offset().top;
+//     var distanceFromBottom = offset - scroll - screenHeight
+//
+//     if (offset > screenHeight && offset) {
+//       $(this).css('background-position', 'center ' + (( distanceFromBottom  ) * 0.2) +'px');
+//     } else {
+//       $(this).css('background-position', 'center ' + (( -scroll ) * 0.01) + 'px');
+//     }
+//   })
+// }
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: ($(hash).offset().top)
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
