415da7b6fbcc51a286e2f8a6c80bfbfd01dc6893
Rajamuthukumaran <rajamuthukumaran333@gmail.com>
2018-03-02 16:22:00 +0530

    eat-a-thon

--- eatathon.php
+++ eatathon.php
@@ -7,11 +7,21 @@
 
   <?php
     $head = "Eat-a-thon";
-    $desc = "";
+    $desc = "Not good at anything other than eating? don't worry we got you covered, try our Eat-a-thon";
     $duration = "";
     $participant = "";
     include("includes/desc.php");
   ?>
+
+  <section class="rules">
+  	<ul>
+  		<li>Only for those registered before 1st - 15th of march, First come</li>
+  		<li>Possible edible things will be given for participants</li>
+  		<li>Participants will be selected for 2nd round based on first round</li>
+  		<li>Judge's decision will be final no arguments</li>
+  		<li>Disclaimer: The committe will not responsible for any of your health issues</li>
+  	</ul>
+  </section>
 
 </main>
 
