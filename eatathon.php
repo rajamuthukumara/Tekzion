0ca98986dc8ea08a60021c2dc86321aa00cc3a06
Rajamuthukumaran <rajamuthukumaran333@gmail.com>
2018-03-03 09:21:05 +0530

    parallax framework removed

--- eatathon.php
+++ eatathon.php
@@ -1,21 +1,22 @@
 <?php
   $lstyle = "css/eat.css";
-  include("includes/header.css");
+  include("includes/header.php");
 ?>
 
 <main>
 
   <?php
     $head = "Eat-a-thon";
-    $desc = "Not good at anything other than eating? don't worry we got you covered, try our Eat-a-thon";
-    $duration = "";
-    $participant = "";
+    $desc = "Not good at anything other than eating? don't worry we got you covered, try our Eat-a-thon.";
+    $duration = "-";
+    $participant = "1";
     include("includes/desc.php");
   ?>
 
   <section class="rules">
+	<h1>Rules</h1>
   	<ul>
-  		<li>Only for those registered before 1st - 15th of march, First come</li>
+  		<li>Only for those registered before 1st - 15th of march, First come first out will be followed.</li>
   		<li>Possible edible things will be given for participants</li>
   		<li>Participants will be selected for 2nd round based on first round</li>
   		<li>Judge's decision will be final no arguments</li>
--- wd.php
+++ wd.php
@@ -1,17 +1,28 @@
 <?php
   $lstyle = "wd.css";
-  include("includes/header");
+  include("includes/header.php");
 ?>
 
 <main>
 
   <?php
     $head = "Web Development";
-    $desc = "";
-    $duration = "";
-    $participant = "";
+    $desc = "Programming today is a task between software engineers to make the system to communicate with people. Fill the page with stuff and design to attract people with open minded.";
+    $duration = "30 mins";
+    $participant = "2";
     include("includes/desc.php");
   ?>
+
+  <section class="rules">
+  	<h1>Rules</h1>
+  	<ul>
+  		<li>No mobile phone or any electrical devices allowed.</li>
+  		<li>Using internet or discussing with other then your team member is not allowed.</li>
+  		<li>30 min is the maxium duration, judges will only validate what you have done in this 30 mins no matter if you are completed or not.</li>
+  		<li>Participants can use paper for wireframe.</li>
+  		<li>Judge's decision will be final.</li>
+  	</ul>
+  </section>
 
 </main>
 
